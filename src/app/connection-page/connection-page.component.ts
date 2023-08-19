aa349a1704c68b0bf8b570f979f2edc938ed1f76
guilhermeboing <guilhermeboing@pactosolucoes.com.br>
2023-08-19 00:36:58 -0300

    mais atualizacoes

--- src/app/connection-page/connection-page.component.ts
+++ src/app/connection-page/connection-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-connection-page',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connection-page.component.scss']
 })
 export class ConnectionPageComponent implements OnInit {
+
+  @Input() methodData: 'input' | 'output';
+  selectedService;
+  selectData(serviceName: string) {
+    console.log(this.methodData,serviceName)
+    this.selectedService = serviceName
+  }
 
   constructor() { }
 
